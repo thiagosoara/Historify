f82c0d283f1dbe95194fc4cc24561e47dc2929da
Lucaas031 <70085137+Lucaas031@users.noreply.github.com>
2020-09-21 13:41:46 -0300

    Menu
    Design menu

---
+++ Assets/Inputs/Move_Horizon_Tween.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Move_Horizon_Tween : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform img;
+    [SerializeField]
+    private float pos;
+    [SerializeField]
+    private int duract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<RectTransform>();
+        StartCoroutine(MoveX());
+    }
+
+    IEnumerator MoveX()
+    {
+        yield return new WaitForSeconds(4);
+        img.DOAnchorPosX(pos, duract, true);
+    }
+}
---
+++ Assets/Inputs/Move_Tween.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Move_Tween : MonoBehaviour
+{
+    [SerializeField]
+    private Button btn;
+    [SerializeField]
+    private Text txtBtn;
+    [SerializeField]
+    private int duract;
+    [SerializeField]
+    private float pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        txtBtn = GetComponentInChildren<Text>();
+        Movecima();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Movecima()
+    {
+        //Mover
+        btn.GetComponent<RectTransform>().DOAnchorPosY(pos, duract, true);
+    }
+}
