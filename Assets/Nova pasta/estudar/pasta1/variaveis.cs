37c88af97564e83f8baae054bf914334ff8fb955
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-03-10 14:09:46 -0300

    face 0

---
+++ Assets/Nova pasta/estudar/pasta1/variaveis.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variaveis : MonoBehaviour{
+
+    //int carteira = 100;
+    /*float carteira2 = 10.5f;
+    bool teste = false;
+    string texto;*/
+
+    //int num1,num2,num3,resultado;
+
+    //int carteira;
+    //int numero1=10;
+    //int numero2=20;
+
+    int moedasHeroi=20;
+    int valorVida=100;
+    String result;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*print(carteira);
+        print(carteira2);
+        print (teste);
+        texto = carteira.ToString();
+        print (texto + 1);*/
+        
+        //num1=10;
+        //num2=20;
+        //num3=30;
+        //resultado= num1 + num2+ num3;
+        //carteira=10;
+        //carteira +=2;
+
+        //print(!(numero1 == numero2));
+        //print(numero1 != numero2);
+        result=(moedasHeroi>=valorVida)?"Posso compra a vida":"Não tenho dinheiro";
+        print(result);
+        
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/historia em quadrinhos/Inputs/Tween_move_hori.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Tween_move_hori : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform img;
+    [SerializeField]
+    private float pos;
+    [SerializeField]
+    private int duract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<RectTransform>();
+        StartCoroutine(MoveX());
+    }
+
+    // Update is called once per frame
+    IEnumerator MoveX()
+    {
+        yield return new WaitForSeconds(4);
+        img.DOAnchorPosX(pos, duract, true);
+    }
+}
