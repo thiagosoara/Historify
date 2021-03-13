903bb3e6284c4ad621ffb72072e6dc272aca1476
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-03-13 20:24:15 -0300

    quadrinho

---
+++ Assets/historia em quadrinhos/Inputs/TweenLOGOMARCA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TweenLOGOMARCA : MonoBehaviour
+{
+    private Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        //efeitos
+        img.DOFade(0, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
