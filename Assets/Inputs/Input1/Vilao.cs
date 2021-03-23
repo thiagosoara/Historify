9b6bf3bf495d3dabd6e8d2c836f4ae9354ff283f
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-03-23 14:08:52 -0300

    23.04.20

---
+++ Assets/Inputs/Input1/Vilao.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vilao : MonoBehaviour
+{
+    public float vel = 1.5f;
+    public bool liberaPer = false;
+    public float distancia;
+    public Transform Hero;
+    public bool face = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia = Vector2.Distance(this.transform.position, Hero.transform.position);
+        // flip
+        if((Hero.transform.position.x > this.transform.position.x) && !face)
+        {
+            flip();
+        }
+        else if ((Hero.transform.position.x < this.transform.position.x)&& face)
+        {
+            flip();
+        }
+
+
+        if ((liberaPer == true) && distancia > 2.8f)
+        {
+            if (Hero.transform.position.x < this.transform.position.x)
+            {
+                transform.Translate (new Vector2 (-vel *  Time.deltaTime, 0));
+            }
+            else if (Hero.transform.position.x > this.transform.position.x){
+                transform.Translate(new Vector2 (vel * Time.deltaTime, 0));
+            }
+        }
+    }
+    void flip ()
+    {
+        face = !face;
+        Vector3 scala = this.transform.localScale;
+        scala.x += -1;
+        this.transform.localScale = scala;
+
+    }
+    void OnTriggerEnter2D(Collider2D outro){
+        if (outro.gameObject.CompareTag("Player"))
+        {
+            liberaPer = true;
+        }
+    }
+}
