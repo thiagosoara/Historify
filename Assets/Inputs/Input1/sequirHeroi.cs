ac05b0d95cfb3e9991acce147f8ce75e60313ec5
nadhon <70290966+nadhon@users.noreply.github.com>
2021-03-24 15:10:46 -0300

    update

--- Assets/Inputs/Input1/sequirHeroi.cs
+++ Assets/Inputs/Input1/sequirHeroi.cs
@@ -7,10 +7,15 @@ public class sequirHeroi : MonoBehaviour
     public float Speed;
     private Transform Target;
 
+    public Animation anim;
+
+    public bool vivo;
+
     // Start is called before the first frame update
     void Start()
     {
         Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim= GetComponent<Animation>();
     }
 
     // Update is called once per frame
