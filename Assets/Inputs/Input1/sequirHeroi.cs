326b37363e63fc9d1a1314bf04e580ad5ee1defb
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-03-24 15:46:33 -0300

    24.03

--- Assets/Inputs/Input1/sequirHeroi.cs
+++ Assets/Inputs/Input1/sequirHeroi.cs
@@ -7,6 +7,7 @@ public class sequirHeroi : MonoBehaviour
     public float Speed;
     private Transform Target;
     public float distancia;
+    //public bool face = true;
     
 
     // Start is called before the first frame update
@@ -20,5 +21,37 @@ public class sequirHeroi : MonoBehaviour
     {
         if (Vector2.Distance(transform.position, Target.position) > distancia){
         transform.position = Vector2.MoveTowards(transform.position,Target.position, Speed * Time.deltaTime);}
+
+
+       /*// flip
+        if((Target.transform.position.x > this.transform.position.x) && !face)
+        {
+            flip();
+        }
+        else if ((Target.transform.position.x < this.transform.position.x)&& face)
+        {
+            flip();
+        }
+
+
+         if (Target.transform.position.x < this.transform.position.x)
+            {
+                transform.Translate (new Vector2 (-Speed *  Time.deltaTime, 0));
+                
+            }
+            else if (Target.transform.position.x > this.transform.position.x){
+                transform.Translate(new Vector2 (Speed * Time.deltaTime, 0));
+                
+            }*/
+            
     }
+
+    /*void flip ()
+    {
+        face = !face;
+        Vector3 scala = this.transform.localScale;
+        scala.x += -1;
+        this.transform.localScale = scala;
+
+    }*/
 }
