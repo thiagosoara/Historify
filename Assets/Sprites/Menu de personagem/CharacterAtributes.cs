2fca53912a9ea172ba3a04291edf623b0797d8ae
nadhon <70290966+nadhon@users.noreply.github.com>
2021-04-05 13:58:22 -0300

    update menu de personagem

---
+++ Assets/Sprites/Menu de personagem/CharacterAtributes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class CharacterAtributes : MonoBehaviour
+{
+
+    [Header("Mateu")]
+   public string Mateu;
+   
+   [Header("Vida")]
+   public int currentVida;
+   public int maxvida;
+
+   [Header("Stamina")]
+
+   public int currentStamina;
+
+   public int maxStamina;
+
+   [Header("Status")]
+
+   public int forca = 1;
+   public int resistencia = 1;
+   public int dano = 1;
+   public int defesa = 1;
+   public float velocidade = 2f;
+}
