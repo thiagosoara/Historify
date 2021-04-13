c668ce1cfe23daf93f586630fcbb5b6c064c046a
Francievellyn <fls@a.palmares.ifpe.edu.br>
2021-04-13 15:39:12 -0300

    corrigingo conflitos

--- Assets/Sprites/Menu de personagem/CharacterMenu.cs
+++ Assets/Sprites/Menu de personagem/CharacterMenu.cs
@@ -1,12 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 //using Unity.UI;
-=======
-using System;
-using UnityEngine.UI;
->>>>>>> 98b9cb5262e9d146a0f2b9cd8757e8a6107baf3a
 
 public class CharacterMenu : MonoBehaviour
 {
