bdc04ed84cda366bf37f83f88227cfa27cdd8bf0
nadhon <70290966+nadhon@users.noreply.github.com>
2021-05-15 15:40:51 -0300

    update 15/05/21
    inicio de programação de perseguição

---
+++ Assets/alerta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alerta : MonoBehaviour
+{
+    private bool liberaPer;
+
+    private void OnTriggerEnter2D (EdgeCollider2D alerta){
+        if (alerta.gameObject.CompareTag("Player"))
+             liberaPer = true;
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
