1a3a75307e6ef5d9f7065baa83593519b3b59f81
nadhon <70290966+nadhon@users.noreply.github.com>
2022-06-02 15:59:44 -0300

    uploade de revisão

---
+++ Assets/Firebase/firebase.cs
@@ -0,0 +1,16 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class firebase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+       
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+
+        };
+    }
+}
