5f43b0aecc3bc38d374189153afdf725329bcc99
nadhon <70290966+nadhon@users.noreply.github.com>
2022-06-06 10:44:05 -0300

    update master de personagem

---
+++ Assets/Firebase/FirebaseInit.cs
@@ -0,0 +1,12 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task )
+    }
+}
