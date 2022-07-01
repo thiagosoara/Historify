93780c11cec6f055efe7c788584d6c04a8f3e995
nadhon <70290966+nadhon@users.noreply.github.com>
2022-07-01 12:08:22 -0300

    adição de um script simples de inventario organizável

---
+++ Assets/Inputs/Script de itens/Centralizar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Centralizar : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position +=(transform.parent.position - transform.position) *5* Time.deltaTime;
+
+    }
+}
---
+++ Assets/Inputs/Script de itens/Curativos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curativos : MonoBehaviour
+{
+     public int vida;
+
+    private void OnCollisionEnter2D(Collision2D colision)
+    {
+
+        if(colision.gameObject.name.Equals("babosa"))
+        {
+            Destroy(colision.gameObject);
+            vida++;
+        }
+    }
+}
