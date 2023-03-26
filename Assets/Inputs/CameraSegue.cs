37ce593eba193f02bf86ebaa36c1e21f07a6412d
Francievellyn <fls@a.palmares.ifpe.edu.br>
2023-03-26 09:06:39 -0300

    camera segue naçu

--- Assets/Inputs/CameraSegue.cs
+++ Assets/Inputs/CameraSegue.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class CameraSegue : MonoBehaviour
 {
     public GameObject player;
+    public GameObject HeroiN;
     public float canVel = 0.25f;
     private bool segueHeroi;
     public Vector3 ultimoAlvoPos;
+    public Vector3 ultimoAlvoPosN;
     public Vector3 velAtual;
+
     // Start is called before the first frame update
     void Start()
     {
         segueHeroi = true;
         ultimoAlvoPos = player.transform.position;
+
+        ultimoAlvoPosN = HeroiN.transform.position;
     }
      void FixedUpdate()
     {
@@ -25,6 +30,14 @@ public class CameraSegue : MonoBehaviour
                 transform.position = new Vector3(novaCamPos.x, novaCamPos.y, transform.position.z);
                 ultimoAlvoPos = player.transform.position;
             }
+            if(player.activeInHierarchy == false){
+                if (HeroiN.transform.position.x >= transform.position.x){
+                    Vector3 novaCamPos = Vector3.SmoothDamp(transform.position, HeroiN.transform.position, ref velAtual, canVel);
+                    transform.position = new Vector3 (novaCamPos.x, novaCamPos.y, transform.position.z);
+                    ultimoAlvoPosN = HeroiN.transform.position;
+                }
+            }
+
         }
     }
 
