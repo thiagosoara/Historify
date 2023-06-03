f1eb8fbfcf9b3e9f9038803ff50e22fca669ed2a
Francievellyn <fls@a.palmares.ifpe.edu.br>
2023-06-03 15:06:07 -0300

    mudança na camera

--- Assets/Inputs/CameraSegue.cs
+++ Assets/Inputs/CameraSegue.cs
@@ -8,42 +8,33 @@ public class CameraSegue : MonoBehaviour
     public GameObject HeroiN;
     public float canVel = 0.25f;
     private bool segueHeroi;
-    public Vector3 ultimoAlvoPos;
-    public Vector3 ultimoAlvoPosN;
-    public Vector3 velAtual;
+    private Vector3 ultimoAlvoPos;
+    private Vector3 velAtual;
 
-    // Start is called before the first frame update
     void Start()
     {
         segueHeroi = true;
         ultimoAlvoPos = player.transform.position;
-
-        ultimoAlvoPosN = HeroiN.transform.position;
     }
-     void FixedUpdate()
+
+    void FixedUpdate()
     {
         if (segueHeroi)
         {
-            if (player.transform.position.x >= transform.position.x)
+            Vector3 alvoPos;
+            if (player.activeInHierarchy)
             {
-                Vector3 novaCamPos = Vector3.SmoothDamp(transform.position, player.transform.position, ref velAtual, canVel);
-                transform.position = new Vector3(novaCamPos.x, novaCamPos.y, transform.position.z);
-                ultimoAlvoPos = player.transform.position;
+                alvoPos = player.transform.position;
             }
-            if(player.activeInHierarchy == false){
-                if (HeroiN.transform.position.x >= transform.position.x){
-                    Vector3 novaCamPos = Vector3.SmoothDamp(transform.position, HeroiN.transform.position, ref velAtual, canVel);
-                    transform.position = new Vector3 (novaCamPos.x, novaCamPos.y, transform.position.z);
-                    ultimoAlvoPosN = HeroiN.transform.position;
-                }
+            else
+            {
+                alvoPos = HeroiN.transform.position;
             }
 
+            Vector3 novaCamPos = Vector3.SmoothDamp(transform.position, alvoPos, ref velAtual, canVel);
+            transform.position = new Vector3(novaCamPos.x, novaCamPos.y, transform.position.z);
+
+            ultimoAlvoPos = alvoPos;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
