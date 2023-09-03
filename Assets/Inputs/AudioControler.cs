3b240403e05eec90db86161b4a517b143b3be508
nadhon <70290966+nadhon@users.noreply.github.com>
2023-09-03 13:37:58 -0300

    adição de um áudio controlador para as fases e efeitos sonoros

---
+++ Assets/Inputs/AudioControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControler : MonoBehaviour
+{
+    public AudioSource audioSorceMusicaDeFundo;
+    public AudioClip[] musicasDeFundo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioClip musicasDeFundoDessaFase = musicasDeFundo[0];
+        audioSorceMusicaDeFundo.clip = musicasDeFundoDessaFase;
+        audioSorceMusicaDeFundo.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
