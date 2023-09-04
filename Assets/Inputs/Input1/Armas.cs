239b75ec333e0099f362537ffb2967dc15ee5c2a
nadhon <70290966+nadhon@users.noreply.github.com>
2023-09-03 21:46:53 -0300

    update

---
+++ Assets/Inputs/Input1/Armas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armas : ScriptableObject 
+{
+    public int itemID;
+    public string weaponName;
+    public int damage;
+    public Sprite image;
+    public AnimationClip animation;
+}
---
+++ Assets/Inputs/Script de itens/Weapons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Weapons : ScriptableObject
+{
+   public string id;
+   public string name;
+   public int dano;
+   public Sprite Imagem;
+   public AnimationClip animation;
+}
