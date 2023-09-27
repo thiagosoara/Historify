bc428f3a3c35992ee7a4470900a7c4f62c01dcd4
nadhon <70290966+nadhon@users.noreply.github.com>
2023-09-27 09:11:33 -0300

    update

---
+++ Assets/Sprites/capataz/EnimyObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "enemyAtributes", menuName="Enemy/New Enemy")]
+public class EnimyObject : ScriptableObject
+{
+    public string name;
+    public int health;
+    public float speed;
+}
