97185327554203f893ae8ad5a79e8910fb6bbf72
nadhon <70290966+nadhon@users.noreply.github.com>
2023-10-16 16:02:18 -0300

    update de imputes relacionados ao jogo

---
+++ Assets/Inputs/Input1/Cacador.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour
+{
+    public Transform player; // Referência ao jogador
+    public float moveSpeed = 5.0f; // Velocidade de movimento
+    public float rotationSpeed = 2.0f; // Velocidade de rotação
+    public GameObject gunPrefab; // Prefab da arma
+    public int initialAmmo = 10; // Munição inicial
+
+    private Transform gunSpawnPoint;
+    private int currentAmmo;
+
+    void Start()
+    {
+        gunSpawnPoint = transform.Find("GunSpawnPoint");
+        currentAmmo = initialAmmo;
+    }
+
+    void Update()
+    {
+        // Rotação para encarar o jogador
+        Vector3 playerDirection = player.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(playerDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+
+        // Movimento em direção ao jogador
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+        // Disparar quando o jogador estiver à vista
+        if (IsPlayerInSight())
+        {
+            if (currentAmmo > 0)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    bool IsPlayerInSight()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, player.position - transform.position, out hit))
+        {
+            if (hit.transform == player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void Shoot()
+    {
+        // Lógica de disparo (instanciação de projéteis, controle de munição, causar dano ao jogador, etc.)
+    }
+
+    void Die()
+    {
+        // Lógica para quando o caçador morrer (deixar arma e munição)
+        Instantiate(gunPrefab, transform.position, Quaternion.identity);
+        // Deixe munição com a arma
+        gunPrefab.GetComponent<Weapon>().SetAmmo(currentAmmo);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Inputs/cacado1.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cacado1 : MonoBehaviour
+{
+public float speed = 2f; // Velocidade de movimento do inimigo
+    public Transform pointA; // Ponto de início da patrulha
+    public Transform pointB; // Ponto final da patrulha
+    public Animator anim;
+    public GameObject apresentacao;
+    public GameObject escolha;
+
+    public GameObject chuva;
+
+    public GameObject andando;
+    public GameObject parado;
+  
+
+
+
+    private bool movingRight = true; // Variável que controla a direção de movimento do inimigo
+
+    // Update é chamado uma vez por quadro
+    void Update()
+    {
+        // Se o inimigo estiver se movendo para a direita
+        if (movingRight)
+        {
+            // Move o inimigo para a direita ao longo do eixo X
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            anim.SetBool("Idle", true);
+            anim.SetBool("Run", false);
+      
+
+            // Se o inimigo atingiu o ponto final da patrulha
+            if (transform.position.x >= pointB.position.x)
+            {
+                // Inverte a direção de movimento do inimigo
+                movingRight = false;
+                // Inverte a direção em que o inimigo está olhando
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+
+            }
+            
+        }
+        // Se o inimigo estiver se movendo para a esquerda
+        else 
+        {
+            // Move o inimigo para a esquerda ao longo do eixo X
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            anim.SetBool("Idle", true);
+            anim.SetBool("Run", false);
+    
+            // Se o inimigo atingiu o ponto de início da patrulha
+            if (transform.position.x <= pointA.position.x)
+            {
+                // Inverte a direção de movimento do inimigo
+                movingRight = true;
+                // Inverte a direção em que o inimigo está olhando
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            
+        }
+
+    }
+
+    // Detecta colisões com outros objetos
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Se o inimigo colidiu com o jogador
+        if (other.CompareTag("Player"))
+        {
+   
+            // Executa a lógica do jogo correspondente (por exemplo, reduzindo a saúde do jogador)
+            // ...
+            if(apresentacao.activeInHierarchy == false){
+                apresentacao.SetActive(true);
+       
+            }
+            // Faz o inimigo parar de se mover por 2 segundos antes de atacar novamente
+            StartCoroutine(StopForSeconds(2));
+
+        }
+    }
+
+    // Faz o inimigo parar de se mover por um determinado número de segundos
+    IEnumerator StopForSeconds(float seconds)
+    {
+        // Para o movimento do inimigo
+        speed = 0f;
+
+
+        // Espera o número de segundos especificado
+        yield return new WaitForSeconds(seconds);
+        // Restaura o movimento do inimigo
+        if (apresentacao.activeInHierarchy== true){
+            apresentacao.SetActive(false);
+        }
+        if(apresentacao.activeInHierarchy == false){
+            if (escolha.activeInHierarchy == false){
+                escolha.SetActive(true);
+                chuva.SetActive(false);
+                
+            }
+        }
+        speed = 0f;
+
+
+    
+
+    }
+}
+
