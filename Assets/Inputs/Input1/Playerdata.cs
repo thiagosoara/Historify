68eb41e4c562224e12024c478a3ad3b49cca06b1
nadhon <70290966+nadhon@users.noreply.github.com>
2023-10-15 23:45:13 -0300

    update de animação e comandos para serem implementados

---
+++ Assets/Inputs/Input1/Playerdata.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="ScriptableObject/Player")]
+
+public class Playerdata : ScriptableObject
+{
+   public string Playername;
+
+   public Sprite image;
+
+}
