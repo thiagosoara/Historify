03f7580aa488d46d180b0237ec5c102dc39d4367
nadhon <70290966+nadhon@users.noreply.github.com>
2024-09-26 09:06:39 -0300

    outra modificação que não sei se via ficar bom

---
+++ Assets/Inputs/AttackPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+    private bool atacando;
+    public Animato animator;
+
+    public Transform ataquePoint;
+
+    public float ataqueRanger= 0.5f;
+
+    public LayerMask enemyLayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        atacando = Input.GetbuttonDown("Fire2");
+        if(atacando == true){
+            Ataque()
+        }
+        void Ataque()
+        {
+            animator.SetTrigger("ataque")
+        }
+    }
+}
